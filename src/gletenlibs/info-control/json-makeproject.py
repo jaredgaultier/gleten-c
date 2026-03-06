39761c6e1c2a9baede2a80df38b6aa44a50aa559
Jared Gastelum <cd135_su@yahoo.com>
2026-03-05 21:38:03 -0700

    Title, description and author now have char limit after input

--- src/gletenlibs/info-control/json-makeproject.py
+++ src/gletenlibs/info-control/json-makeproject.py
@@ -1,6 +1,10 @@
 import json
 import os
 
+TITLE_LENGTH_MAX = 30
+DESCRIPTION_LENGTH_MAX = 500
+AUTHOR_LENGTH_MAX = 20
+
 def main(project_title, project_description, project_author):
     
     project_info = {
@@ -21,6 +25,17 @@ if __name__ == "__main__":
     _project_description = str(input("Project description: "))
     _project_author = str(input("Author: "))
 
+    if len(_project_title) > TITLE_LENGTH_MAX:
+        print(f"Project title exceeds the ({TITLE_LENGTH_MAX}) letters limit")
+        exit();
+
+    if len(_project_description) > DESCRIPTION_LENGTH_MAX:
+        print(f"Project title exceeds the ({DESCRIPTION_LENGTH_MAX}) letters limit")
+        exit();
+    
+    if len(_project_author) > AUTHOR_LENGTH_MAX:
+        print(f"Project title exceeds the ({AUTHOR_LENGTH_MAX}) letters limit")
+        exit();
 
     print("Making a new project...")
     main(_project_title, _project_description, _project_author)
