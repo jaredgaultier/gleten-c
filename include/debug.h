316f0d69236d1c31b517abcaa7cbd619e8d88e0e
Jared Gastelum <cd135_su@yahoo.com>
2026-03-05 17:48:35 -0700

    Header for DEBUG boolean variable was made and avoid to put it in every file

---
+++ include/debug.h
@@ -0,0 +1,15 @@
+/*
+Set DEBUG to true if wanna look for
+array iterations and further information
+about loops, sizes, and more.
+*/
+
+
+#ifndef DEBUG_H
+#define DEBUG_H
+
+#include <stdbool.h>
+
+static const bool DEBUG = true;
+
+#endif
