ae6e315d8166f9c52a5bf4d3aaed7f52bbb3cea7
Jared Gastelum <cd135_su@yahoo.com>
2026-03-05 17:40:04 -0700

    Uploading headers

---
+++ include/command-list.h
@@ -0,0 +1,21 @@
+#ifndef COMMAND_LIST_H
+#define COMMAND_LIST_H
+
+#include <stddef.h>
+
+static char const *commandList[] = {
+    "makefile",
+    "deletefile",
+
+    "fileExists",
+
+    "makedir",
+    "deletedir",
+    "dirExists",
+
+    "editWallpaper",
+    "if",
+    NULL
+};
+
+#endif
