32e040efaed50a6d12c768aaf492eba09b7046e7
Jared Gastelum <cd135_su@yahoo.com>
2026-03-10 16:36:44 -0700

    WIP, char reading function

--- src/panel/panel.c
+++ src/panel/panel.c
@@ -43,16 +43,45 @@ int displayPanel(char const *projectTitle){
             printf("%zu: \n", (size_t)i);
 
 
-    // tf
+
+    // avoids stdin trash
     getchar();
-    printf("\n\n\n>> ");
+
+
+
+
+    printf("\n>> ");
 
     while(strcmp(_opc, "exit") != 0){
-        _gotoxy(4, 15);printf("%s", cosmetic_longSpace);_gotoxy(4, 15);
+        _gotoxy(4, 13);printf("%s", cosmetic_longSpace);_gotoxy(4, 13);
 
         // clearCmd();
         fgets(_opc, sizeof(_opc), stdin);
         removeNewLine(_opc, sizeof(_opc));
+
+        /* 
+        SCHEMA
+
+        
+        If the user input (_opt) is not in the command list,
+        the program tries to compare it with the other commands
+        in the list. If the stringLikeness function returns 1,
+        likenessFind will show the user the closest command.
+
+        If the user input is in the command list, the program
+        checks whether the command is meant to display help
+        information. The userHelp function compares _opc
+        (as a parameter) with various keywords and shows
+        the corresponding information.
+
+        Otherwise, the program checks whether the command
+        requires a parameter. If so, it asks the user for it.
+
+        Finally, if everything is correct, the command
+        (and its parameter, if any) is added to the
+        commandBuffer.
+        
+        */
 
         if (inCommandList(_opc, commandList) == false){
             
@@ -64,6 +93,66 @@ int displayPanel(char const *projectTitle){
                 userHelp(_opc);
 
             } else {
+
+                if (inCommandList(_opc, parameterCommandList)){
+
+                    printf("Parameter: ");
+                    _gotoxy(0, 15);
+
+                    // dynamic argBuffer[100];
+                    char *argBuffer = malloc(100);
+
+                    argBuffer[0] = '\"';
+
+
+                    printf("1: %s", _opc);
+
+                    char _cArg;
+                    int i;
+
+
+                    i = 1;
+                    while ((_cArg = getchar()) != '\n'){
+                        if (i >= 98 ){
+
+                            char *temp = realloc(argBuffer, i + 1);
+
+                            argBuffer = temp;
+
+                        }
+                        
+                        argBuffer[i] = _cArg;
+                        i++;
+                    }
+                    
+                    /*
+                    Let space to '\0' and '\"' in case
+                    
+                    the argument needed it
+                    
+                    argBuffer[limit + 1] = '\"'
+                    
+                    argBuffer[limit + 2] = '\0'
+                    */
+                    char *temp = realloc(argBuffer, i + 3);
+
+                    argBuffer = temp;
+
+
+                    printf("JOE BIDEN:%d", i);
+                    getchar();
+                    argBuffer[i + 1] = '\"';
+                    argBuffer[i + 2] = '\0';
+
+                    // Final result must be like this
+                    // function"parameter"\0
+
+
+                    strcat(_opc, argBuffer);
+
+
+                    free(argBuffer);
+            }
                 
                 char **temp = realloc(commandBuffer, (n + 1) * sizeof(char*));
                 
@@ -79,14 +168,15 @@ int displayPanel(char const *projectTitle){
                 
 
                 cosmeticXAxisCleaning(16, 25);
-
                 _gotoxy(3, n + 3);
-            
                 printf(" %s", commandBuffer[n]);
                 
                 n++;
             }
+
         }
+        
+        memset(_opc, 0, sizeof(_opc));
 
     }
 
