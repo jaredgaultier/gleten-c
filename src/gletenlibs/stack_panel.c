33bd5918440c734be56fc03e34bcf934e680819b
Jared Gastelum <cd135_su@yahoo.com>
2026-03-17 17:31:07 -0700

    Used for stack data structure (only for char datatype)

--- src/gletenlibs/stack_panel.c
+++ src/gletenlibs/stack_panel.c
@@ -2,25 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-typedef struct _stack {
-
-    char **elements;
-    int count_elements;
+#include "../../include/stack.h"
 
 
-} stack;
 
 void stackConstructor(stack *__a){
 
     __a->elements = NULL;
-    __a->count_elements = 0;
+    __a->countElements = 0;
 }
 
 void push(stack *st, char *value){
-    int n = 0;
 
-    char **temp = realloc(st->elements, (st->count_elements + 1) * sizeof(*st->elements));    
+    char **temp = realloc(st->elements, (st->countElements + 1) * sizeof(*st->elements));    
 
     if (temp == NULL){
         printf("Memory error!");
@@ -28,24 +22,24 @@ void push(stack *st, char *value){
     }
 
 
-    st->elements = temp;
-    st->elements[st->count_elements] = malloc(strlen(value) + 1);
-    strcpy(st->elements[st->count_elements], value);
+    st -> elements = temp;
+    st -> elements[st -> countElements] = malloc(strlen(value) + 1);
+    strcpy(st -> elements[st -> countElements], value);
 
-    st->count_elements++;
+    st->countElements++;
 }
 
 void pop(stack *st, char *value){
-    for (size_t i = 0;i < st->count_elements;i++){
+    for (size_t i = 0;i < (size_t)st -> countElements;i++){
         
         if (strcmp(st->elements[i], value) == 0){
             
             free(st->elements[i]);
             
-            for (size_t j = i;j < st->count_elements-1;j++)
-                st->elements[j] = st->elements[j + 1];
+            for (size_t j = i;j < (size_t)st -> countElements-1;j++)
+                st->elements[j] = st -> elements[j + 1];
 
-            st->count_elements -= 1;
+            st->countElements -= 1;
             break;   
         }
 
@@ -56,7 +50,7 @@ void pop(stack *st, char *value){
 
 void cleanStackMemory(stack *st){
 
-    for (size_t i = 0;i < st->count_elements;i++){
+    for (size_t i = 0;i < (size_t)st -> countElements;i++){
         free(st->elements[i]);
     }
 
