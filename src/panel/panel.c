766ff05c701baee87306d196693be78ecec325e8
Jared Gastelum <cd135_su@yahoo.com>
2026-03-17 17:31:38 -0700

    Reorganizing panel for more comprehensible code

--- src/panel/panel.c
+++ src/panel/panel.c
@@ -12,12 +12,22 @@
 #include "../include/panel.h"
 #include "../include/gleten-string.h"
 #include "../include/command-list.h"
-
+#include "../include/stack.h"
 
 /*
-The commands will be here, in this buffer
-*/
-static char **commandBuffer = NULL;
+typedef struct command {
+    
+    char **commandBuffer;
+    size_t commandCount;
+    size_t commandId;
+
+} c;*/
+
+
+
+// Here will be all the command input by the user
+stack commandStack;
+
 
 void userHelp(char *opt){
     if (strcmp(opt, "help")){
@@ -26,8 +36,6 @@ void userHelp(char *opt){
 }
 
 int displayPanel(char const *projectTitle){
-    // used for memory allocation
-    int n = 0;
     
     size_t editingLine = 1;
 
@@ -37,10 +45,8 @@ int displayPanel(char const *projectTitle){
 
     printf("\t::Editing %s::\n", projectTitle);
     printf("\tEditing line %zu\n", editingLine);
-
-        // The two variables here are y and x, which are the coordinates of the cursor in the terminal, and they are used to print the project title in the center of the terminal.
-        for (size_t i = 1;i < 10; i++)
-            printf("%zu: \n", (size_t)i);
+    for (size_t i = 1;i < 10; i++)
+        printf("%zu: \n", (size_t)i);
 
 
 
@@ -53,138 +59,19 @@ int displayPanel(char const *projectTitle){
     printf("\n>> ");
 
     while(strcmp(_opc, "exit") != 0){
-        _gotoxy(4, 13);printf("%s", cosmetic_longSpace);_gotoxy(4, 13);
+        	_gotoxy(4, 13);printf("%s", cosmetic_longSpace);_gotoxy(4, 13);
 
-        // clearCmd();
-        fgets(_opc, sizeof(_opc), stdin);
-        removeNewLine(_opc, sizeof(_opc));
+        	// clearCmd();
+        	fgets(_opc, sizeof(_opc), stdin);
+        	removeNewLine(_opc, sizeof(_opc));
 
-        /* 
-        SCHEMA
+		// INITS STACK
+		stackConstructor(&commandStack);
 
-        
-        If the user input (_opt) is not in the command list,
-        the program tries to compare it with the other commands
-        in the list. If the stringLikeness function returns 1,
-        likenessFind will show the user the closest command.
-
-        If the user input is in the command list, the program
-        checks whether the command is meant to display help
-        information. The userHelp function compares _opc
-        (as a parameter) with various keywords and shows
-        the corresponding information.
-
-        Otherwise, the program checks whether the command
-        requires a parameter. If so, it asks the user for it.
-
-        Finally, if everything is correct, the command
-        (and its parameter, if any) is added to the
-        commandBuffer.
-        
-        */
-
-        if (inCommandList(_opc, commandList) == false){
-            
-            likenessFind(_opc);
-
-        } else {
-            if (inCommandList(_opc, ignoreCommandList)){
-                
-                userHelp(_opc);
-
-            } else {
-
-                if (inCommandList(_opc, parameterCommandList)){
-
-                    printf("Parameter: ");
-                    _gotoxy(0, 15);
-
-                    // dynamic argBuffer[100];
-                    char *argBuffer = malloc(100);
-
-                    argBuffer[0] = '\"';
-
-
-                    printf("1: %s", _opc);
-
-                    char _cArg;
-                    int i;
-
-
-                    i = 1;
-                    while ((_cArg = getchar()) != '\n'){
-                        if (i >= 98 ){
-
-                            char *temp = realloc(argBuffer, i + 1);
-
-                            argBuffer = temp;
-
-                        }
-                        
-                        argBuffer[i] = _cArg;
-                        i++;
-                    }
-                    
-                    /*
-                    Let space to '\0' and '\"' in case
-                    
-                    the argument needed it
-                    
-                    argBuffer[limit + 1] = '\"'
-                    
-                    argBuffer[limit + 2] = '\0'
-                    */
-                    char *temp = realloc(argBuffer, i + 3);
-
-                    argBuffer = temp;
-
-
-                    printf("JOE BIDEN:%d", i);
-                    getchar();
-                    argBuffer[i + 1] = '\"';
-                    argBuffer[i + 2] = '\0';
-
-                    // Final result must be like this
-                    // function"parameter"\0
-
-
-                    strcat(_opc, argBuffer);
-
-
-                    free(argBuffer);
-            }
-                
-                char **temp = realloc(commandBuffer, (n + 1) * sizeof(char*));
-                
-                if (temp == NULL){
-                    printf("Memory error!");
-                    break;
-                }
-
-                commandBuffer = temp;
-
-                commandBuffer[n] = malloc(strlen(_opc) + 1);
-                strcpy(commandBuffer[n], _opc);
-                
-
-                cosmeticXAxisCleaning(16, 25);
-                _gotoxy(3, n + 3);
-                printf(" %s", commandBuffer[n]);
-                
-                n++;
-            }
-
-        }
-        
-        memset(_opc, 0, sizeof(_opc));
-
-    }
-
-    for (size_t i = 0;i < (size_t)n;i++){
-        free(commandBuffer[i]);
-    }
-
-    free(commandBuffer);
+	
+		// LIBERATES STACK HEAP DATA
+		cleanStackMemory(&commandStack);
+	}
 
     return 0;
 }
