8ef701fdf01f224223259dd9b4088bcfb45b6508
Jared Gastelum <cd135_su@yahoo.com>
2026-04-09 12:52:32 -0700

    (refactor): Dictionary links all commands with their template

---
+++ src/gletenlibs/data-interpreter/command_dict.py
@@ -0,0 +1,9 @@
+from func.makefile import make_file_template
+from func.removefile import remove_file_template
+
+GLETEN_COMMANDS = {
+    "parameter-commands": {
+        "makefile": make_file_template,
+        "removefile": remove_file_template,
+    }
+}
