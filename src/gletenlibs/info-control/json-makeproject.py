373399992dedd5ccd4d45558eb04d24f61511a71
Jared Gastelum <cd135_su@yahoo.com>
2026-04-10 12:33:19 -0700

    refactor: Change input length max structure The code now is more readable and the while structure to check if the user input is into the object char limit, now is a function (input_length_limit).

--- src/gletenlibs/info-control/json-makeproject.py
+++ src/gletenlibs/info-control/json-makeproject.py
@@ -1,11 +1,45 @@
 import json
 import os
 
-TITLE_LENGTH_MAX = 30
-DESCRIPTION_LENGTH_MAX = 500
-AUTHOR_LENGTH_MAX = 20
+TITLE_LENGTH_MAX: int = 30
 
-def main(project_title, project_description, project_author):
+DESCRIPTION_LENGTH_MAX: int = 500
+
+AUTHOR_LENGTH_MAX: int = 20
+
+
+
+def input_length_limit(
+        
+        message: str,
+        limit: int
+
+        ) -> str:
+    
+
+    ans: str = ""
+
+    while ans != "quit":
+        ans = input(message)
+
+        if len(ans) <= limit:
+            break
+
+        else:
+            print(f"Input exceeds the ({limit}) char limit!")
+            pass
+
+
+    return ans
+
+
+
+
+def main(
+        project_title: str,
+        project_description: str, 
+        project_author: str
+        ) -> None:
 
     PROJ_PATH = f"gleten-projects/{project_info["title"]}"
 
@@ -26,29 +60,20 @@ if __name__ == "__main__":
 
     continue_to_main = False
 
-    while True:
-        _project_title = str(input("Project title: "))
-        if len(_project_title) > TITLE_LENGTH_MAX:
-            print(f"Project title exceeds the ({TITLE_LENGTH_MAX}) letters limit")
+    _project_title: str = input_length_limit(
+        "Project title: ",
+        TITLE_LENGTH_MAX
+    )
 
-        else:
-            break
+    _project_description: str = input_length_limit(
+        "Project description: ",
+        DESCRIPTION_LENGTH_MAX
+    )
 
-    while True:    
-        _project_description = str(input("Project description: "))
-        if len(_project_description) > DESCRIPTION_LENGTH_MAX:
-            print(f"Project title exceeds the ({DESCRIPTION_LENGTH_MAX}) letters limit")
-    
-        else:
-            break
-        
-    while True:
-        _project_author = str(input("Author: "))
-        if len(_project_author) > AUTHOR_LENGTH_MAX:
-            print(f"Project title exceeds the ({AUTHOR_LENGTH_MAX}) letters limit")
-
-        else:
-            break
+    _project_author: str  = input_length_limit(
+        "Project author: ",
+        AUTHOR_LENGTH_MAX
+    )
     
                         
 
